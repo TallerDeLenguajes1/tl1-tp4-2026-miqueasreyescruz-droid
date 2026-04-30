9456335130d5d69b1c5b4da7a13df9f586f2c267
MiqueasTablet <miqueasreyescruz@gmail.com>
2026-04-30 18:30:42 -0300

    Agrego la la interfaz para cargar tareas en la lista tPendientes

--- To-Do.c
+++ To-Do.c
@@ -19,8 +19,20 @@ Nodo * crearTarea (int *ID);
 void insertarTarea (Nodo **Start,Nodo *Nodo);
 
 int main () {
-  int ID = ID_INICIAL;
+  int ID = ID_INICIAL, aux;
   Nodo *tPendientes, *tRealizadas;
+
+  tPendientes = crearListaVacia();
+  tRealizadas = crearListaVacia();
+
+  printf("___INGRESANDO TAREAS___\n");
+  do {
+    insertarTarea(&tPendientes,crearTarea(&ID));
+
+    printf("->Desea ingresar otra tarea?\n1. Si\n2. No\n");
+    scanf("%d",&aux);
+  } while (aux == 1);
+
   return 0;
 }
 
@@ -37,14 +49,14 @@ Nodo * crearTarea (int *ID) {
   nNodo -> T.TareaID = *ID;
   (*ID)++;
 
-  printf("Ingrese una descripcion de la tarea: \n");
+  printf("-Ingrese una descripcion de la tarea: \n");
   while(getchar() != '\n');
   fgets(buffer, 100, stdin);
   buffer[strcspn(buffer, "\n")] = 0;
   nNodo -> T.Descripcion = (char*) malloc ((strlen(buffer) + 1) * sizeof(char));
   strcpy(nNodo -> T.Descripcion,buffer);
 
-  printf("Ingrese la duracion de la tarea: \n");
+  printf("-Ingrese la duracion de la tarea: \n");
   scanf("%d",&aux);
   nNodo -> T.Duracion = aux;
 
