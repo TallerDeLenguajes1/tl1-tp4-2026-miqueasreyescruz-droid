798e5c1869209ffc11149594bc5b2ef22adc35ba
MiqueasTablet <miqueasreyescruz@gmail.com>
2026-04-30 16:46:27 -0300

    Agrego la funcion crearTarea a To-Do.c

--- To-Do.c
+++ To-Do.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #define ID_INICIAL 1000
 
 typedef struct Tarea {
@@ -12,8 +14,41 @@ typedef struct Nodo {
   struct Nodo *Siguiente;
 } Nodo;
 
+Nodo * crearListaVacia(); 
+Nodo * crearTarea (int *ID);
+
 int main () {
   int ID = ID_INICIAL;
-  
+  Nodo *tPendientes, *tRealizadas;
+  tPendientes = crearTarea(&ID);
   return 0;
+}
+
+Nodo * crearListaVacia() {
+  return NULL;
+}
+
+Nodo * crearTarea (int *ID) {
+  int aux;
+  char buffer[100];
+  Nodo *nNodo = (Nodo*) malloc (sizeof(Nodo));
+
+  printf("->Creando Tarea\n");
+  nNodo -> T.TareaID = *ID;
+  (*ID)++;
+
+  printf("Ingrese una descripcion de la tarea: \n");
+  while(getchar() != '\n');
+  fgets(buffer, 100, stdin);
+  buffer[strcspn(buffer, "\n")] = 0;
+  nNodo -> T.Descripcion = (char*) malloc ((strlen(buffer) + 1) * sizeof(char));
+  strcpy(nNodo -> T.Descripcion,buffer);
+
+  printf("Ingrese la duracion de la tarea: \n");
+  scanf("%d",&aux);
+  nNodo -> T.Duracion = aux;
+
+  nNodo -> Siguiente = NULL;
+
+  return nNodo;
 }
